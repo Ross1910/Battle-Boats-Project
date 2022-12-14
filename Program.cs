e62da0472b310f8018fa2e251a000c54b8bfd6a0
Ross1910 <Rossgeorgeb@gmail.com>
2022-12-14 09:02:32 +0000

    Outputs initial grid

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace BattleBoatsProject
+{
+    class BattleBoatsProject
+    {
+        static int[] boatLengths = { 1, 1, 2, 2, 3 };
+
+        static void Main(string[] args)
+        {
+            char[,] playerBoatsArray = new char[8, 8];
+            //output the grid for the player to place boats
+            CreatePlacementView();
+            //get the player to place the boats
+            PlayerBoatsPlacement(playerBoatsArray);
+        }
+
+        static void CreatePlacementView()
+        {
+            Console.WriteLine("╔══════════════════╗");
+            Console.WriteLine("║ place your boats ║");
+            Console.WriteLine("║    ╔════════╗    ║");
+            Console.WriteLine("║    ║++++++++║    ║");
+            Console.WriteLine("║    ║++++++++║    ║");
+            Console.WriteLine("║    ║++++++++║    ║");
+            Console.WriteLine("║    ║++++++++║    ║");
+            Console.WriteLine("║    ║++++++++║    ║");
+            Console.WriteLine("║    ║++++++++║    ║");
+            Console.WriteLine("║    ║++++++++║    ║");
+            Console.WriteLine("║    ║++++++++║    ║");
+            Console.WriteLine("║    ╚════════╝    ║");
+            Console.WriteLine("╚══════════════════╝");
+
+        }
+
+        static char[,] PlayerBoatsPlacement(char[,] playerBoatsArray)
+        {
+            for (int i = 0; i < boatLengths.Length; i++)
+            {
+
+            }
+            return playerBoatsArray;
+        }
+    }
+}
